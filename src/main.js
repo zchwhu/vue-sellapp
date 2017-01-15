3baea0a97b1df54eaf387a99f75dd3e380c83581
zch0301 <291053953@qq.com>
2017-01-15 14:21:13 +0800

    项目入口js，引入vue-router、vue-resource及其他组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue';
 import App from './App';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import goods from './components/goods/goods';
+import seller from './components/seller/seller';
+import ratings from './components/ratings/ratings';
+import './common/stylus/index.styl';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
 });
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/seller': {
+    component: seller
+  },
+  '/ratings': {
+    component: ratings
+  }
+});
+
+router.start(app, '#app');
+router.go('/goods');
