c3ad0608376382f437cba1376a6f1104c9a32525
zch0301 <291053953@qq.com>
2017-01-15 14:17:28 +0800

    购物车组件

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="shopcart">
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper">
+          <div class="logo">
+            <i class="icon-shopping_cart"></i>
+          </div>
+          <div class="price"></div>
+          <div class="desc"></div>
+        </div>
+      </div>
+      <div class="content-right"></div>
+    </div>
+  </div>
+</template>
+<style lang="stylus" rel="stylesheet/stylus">
+  .shopcart
+    position: fixed
+    width: 100%
+    left: 0
+    bottom: 0
+    z-index: 50
+    height: 48px
+    .content
+      display: flex
+      background: #141d27
+      font-size: 0
+      .content-left
+        flex: 1
+        .logo-wrapper
+          display: inline-block
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          width: 56px
+          height: 56px
+          box-sizing: border-box
+          vertical-align: top
+          border-radius: 50%
+          background: #141d27
+          .logo
+            width: 100%
+            height: 100%
+            border-radius: 50%
+            background: #2b343c
+            text-align: center
+            .icon-shopping_cart
+              line-height: 44px
+              font-size: 24px
+              color: #80858a
+        .price
+          display: inline-block
+          vertical-align: top
+          margin-top: 12px
+          box-sizing: border-box
+          line-height: 24px
+          padding-right: 12px
+          border-right: 1px solid rgba(255, 255, 255, 0.1)
+          font-weight: 700
+          font-size: 16px
+        .desc
+          display; inline-block
+      .content-right
+        flex: 0 0 105px
+</style>
+<script type="text/ecmascript-6">
+  export default {};
+</script>
